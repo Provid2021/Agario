c6e27a80bea3287de56e0dc082e02edb4efd94c7
Provid2021 <91719706+Provid2021@users.noreply.github.com>
2022-01-12 16:49:21 +0100

    Add files via upload

---
+++ classe.py
@@ -0,0 +1,12 @@
+import random
+
+from pygame.mcth import vector2
+
+import core
+
+
+
+classe proie
+
+      def__init__(self):
+         self
---
+++ core.py
@@ -0,0 +1,11 @@
+import core
+
+
+def setup():
+    print('setup :')
+    core.window_size=[400,400]
+    core
+
+
+
+    for 1
